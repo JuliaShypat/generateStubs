4c804fa0b060d875d94dd1a4a8c3e82fb7a78f46
yshypat <yuliia.shypat@algoteque.com>
2019-06-07 17:22:35 +0200

    Trying to write to file

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,10 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import * as path from 'path';
+import * as fs from 'fs';
+import { URL } from 'url';
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -15,9 +19,21 @@ export function activate(context: vscode.ExtensionContext) {
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
     // The code you place here will be executed every time your command is executed
-    var currentlyOpenTabfilePath = vscode.window.activeTextEditor!.document;
-    console.log('currentlyOpenTabfilePath', currentlyOpenTabfilePath);
-    const currentFileUri = currentlyOpenTabfilePath.uri;
+    // console.log(vscode.window.activeTextEditor);
+
+    // Get currently opened file path and test spec file
+    const currentlyOpenTabfile = vscode.window.activeTextEditor!.document;
+    const baseFileName = path.basename(currentlyOpenTabfile.fileName, '.ts');
+    const baseFileNameWithExt = path.basename(currentlyOpenTabfile.fileName);
+    const currentFilePath = currentlyOpenTabfile.uri.fsPath;
+    
+    // Spec file
+    const specFileName = baseFileName + '.spec.ts';
+    const fileUrl = new URL(currentFilePath);
+    console.log('currentlyOpenTabfile', currentlyOpenTabfile);
+    console.log('filename', specFileName);
+    console.log(fileUrl);
+    const currentFileUri = currentlyOpenTabfile.uri;
     vscode.workspace.openTextDocument(currentFileUri).then((document) => {
       let text = document.getText();
 
@@ -66,6 +82,40 @@ export function activate(context: vscode.ExtensionContext) {
       console.log('variablesDefinition', variablesDefinition);
       console.log('providersDefinition', providersDefinition);
       console.log('testBedDefinition', testBedDefinition);
+
+
+      let lyrics = 'But still I\'m having memories of high speeds when the cops crashed\n' +  
+             'As I laugh, pushin the gas while my Glocks blast\n' + 
+             'We was young and we was dumb but we had heart';
+
+      // write to a new file named 2pac.txt
+      // fs.writeFile(fileUrl, lyrics, (err) => {  
+      //     // throws an error, you could also catch it here
+      //     if (err) throw err;
+
+      //     // success case, the file was saved
+      //     console.log('Lyric saved!');
+      // });
+      // Open file
+      // fs.open(fileUrl, 'wx', (err, fd) => {
+      //   if (err) {
+      //     if (err.code === 'EEXIST') {
+      //       console.error('myfile already exists');
+      //       return;
+      //     }
+      
+      //     throw err;
+      //   }
+      //   fs.write(fd, buffer, 0, buffer.length, null, (err) => {
+      //       if (err) {
+      //         throw 'error writing file: ' + err;
+      //       }
+      //       fs.close(fd, function() {
+      //           console.log('wrote the file successfully');
+      //       });
+      //   });
+      // });
+
     });
     // Display a message box to the user
     vscode.window.showInformationMessage('Injects generated');
